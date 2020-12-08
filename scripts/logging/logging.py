4214d2572ca8016f8821dfc0ea4cb9d281cabb0e
Krishna Teja Jillelamudi <51982842+KrishnaTejaJ@users.noreply.github.com>
2020-12-08 11:31:06 +0530

    Update logging.py

--- scripts/logging/logging.py
+++ scripts/logging/logging.py
@@ -8,8 +8,8 @@ from scripts.logging.log_level import LogLevel
 class Logging:
 	
 	def log_instances_():
-		#LogInstances.excel2json(Config.repo_file)
-		#LogInstances.json2semgrep(Config.excel2repo)
+		LogInstances.excel2json(Config.repo_file)
+		LogInstances.json2semgrep(Config.excel2repo)
 		LogInstances.semgrep2json()
 
 	def logvnonlog_():
